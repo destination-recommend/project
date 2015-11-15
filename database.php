813e50e5df159c4d1d0aed1436f4b612bbbedb9b
Yizzz <zhang2013yi@gmail.com>
2015-11-15 15:08:02 -0800

    'comment'

--- database.php
+++ database.php
@@ -1,4 +1,12 @@
 <?php
-test git
-wueuuiubhbh
+$mysql_server_name='myeeb542' //mysql数据库服务器
+$mysql_username='myeeb542'; //mysql数据库用户名 
+$mysql_password='myeeb542'; //mysql数据库密码   
+$mysql_database='myeeb542'; //mysql数据库名 
+$con = mysql_connect($mysql_server_name,$mysql_username,$mysql_password); // connect to database
+echo "sucess";
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
 ?>
