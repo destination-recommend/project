82519d0ff0b4bbda922ebba74aaae6eb9a752951
Yizzz <zhang2013yi@gmail.com>
2015-11-15 15:52:09 -0800

    'comment'

--- database.php
+++ database.php
@@ -1,12 +1,12 @@
 <?php
-	$mysql_server_name='myeeb542' //mysql数据库服务器
-	$mysql_username='myeeb542'; //mysql数据库用户名 
-	$mysql_password='myeeb542'; //mysql数据库密码   
-	$mysql_database='myeeb542'; //mysql数据库名 
-	$con = mysql_connect("myeeb542","myeeb542","myeeb542'); // connect to database
-	echo "sucess";
-	if (!$con)
-	  {
-	  die('Could not connect: ' . mysql_error());
-	  }
+$mysql_server_name='yeeb542.c2e3gz3tqbsa.us-west-1.rds.amazonaws.com' //mysql数据库服务器
+$mysql_username='myeeb542'; //mysql数据库用户名 
+$mysql_password='myeeb542'; //mysql数据库密码   
+$mysql_database='myeeb542'; //mysql数据库名 
+$con = mysql_connect($mysql_server_name,$mysql_username,$mysql_password); // connect to database
+echo "sucess";
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
 ?>
