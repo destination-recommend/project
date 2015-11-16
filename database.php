23b6d60494e914eeb3194537f84f833ea9b1b565
Yizzz <zhang2013yi@gmail.com>
2015-11-15 17:53:27 -0800

    'comment'

--- database.php
+++ database.php
@@ -1,17 +1,12 @@
 <?php
-//$mysql_server_name="myeeb542.c2e3gz3tqbsa.us-west-1.rds.amazonaws.com" //mysql数据库服务器
-//$mysql_username="myeeb542"; //mysql数据库用户名 
-//$mysql_password="myeeb542"; //mysql数据库密码   
-//$mysql_database="myeeb542"; //mysql数据库名 
-$con = mysqli_connect($_SERVER['myeeb542.c2e3gz3tqbsa.us-west-1.rds.amazonaws.com'], $_SERVER['myeeb542'], $_SERVER['myeeb542'], $_SERVER['myeeb542'], $_SERVER['3306']);						
-
-
-// Check connection
-if (mysqli_connect_errno($con))
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-  
-echo "sucess";
- 
+$mysql_server_name="myeeb542.c2e3gz3tqbsa.us-west-1.rds.amazonaws.com:3306" //mysql数据库服务器
+$mysql_username="myeeb542"; //mysql数据库用户名 
+$mysql_password="myeeb542"; //mysql数据库密码   
+$mysql_database="myeeb542'; //mysql数据库名 
+$con = mysql_connect($mysql_server_name,$mysql_username,$mysql_password); // connect to database
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+ echo "sucess";
 ?>
