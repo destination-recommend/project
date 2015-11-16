cb4ea50275819aaf688d926386eb2529cb492244
Yizzz <zhang2013yi@gmail.com>
2015-11-15 18:37:25 -0800

    'comment'

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$mysql_server_name="myeeb542.c2e3gz3tqbsa.us-west-1.rds.amazonaws.com:3306" ;
+/*$mysql_server_name="myeeb542.c2e3gz3tqbsa.us-west-1.rds.amazonaws.com:3306" ;
 $mysql_username="myeeb542"; 
 $mysql_password="myeeb542";
 $mysql_database="myeeb542";
@@ -9,11 +9,12 @@ if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
-// $con = mysqli_connect('myeeb542.c2e3gz3tqbsa.us-west-1.rds.amazonaws.com', 'myeeb542', 'myeeb542', 'myeeb542', 3306);	
-// if (mysqli_connect_errno($con))
-// {
-// echo "Failed to connect to MySQL: " . mysqli_connect_error();
-// } 
+  */
+$con = mysqli_connect('myeeb542.c2e3gz3tqbsa.us-west-1.rds.amazonaws.com', 'myeeb542', 'myeeb542', 'myeeb542', 3306);	
+if (mysqli_connect_errno($con))
+ {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ } 
  echo "sucess";
 
 ?>
