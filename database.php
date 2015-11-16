fc25448e14b3c3e8cf52eb44a82170405dbdd45a
Yizzz <zhang2013yi@gmail.com>
2015-11-15 16:13:24 -0800

    'comment'

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$mysql_server_name='myeeb542.c2e3gz3tqbsa.us-west-1.rds.amazonaws.com:3306' //mysql数据库服务器
+$mysql_server_name='10.2.0.7:3306' //mysql数据库服务器
 $mysql_username='myeeb542'; //mysql数据库用户名 
 $mysql_password='myeeb542'; //mysql数据库密码   
 $mysql_database='myeeb542'; //mysql数据库名 
